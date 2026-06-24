812b67f85e16db60eb99fd460ab32ac8c91b2fd3
kainen <kainencf@icloud.com>
2026-06-23 20:51:23 -0700

    life orb functional

--- scripts/lifeOrb.lua
+++ scripts/lifeOrb.lua
@@ -7,7 +7,7 @@ function mod:EvaluateCache(player, cacheFlags)
     if (cacheFlags & CacheFlag.CACHE_DAMAGE) == CacheFlag.CACHE_DAMAGE then
         local itemCount = player:GetCollectibleNum(lifeOrb)
         local multiplierToAdd = lifeOrbMultiplier^itemCount
-        player.Damage = player.Damage * multiplierToAdd
+        player.Damage = player.Damage * (multiplierToAdd)
     end
 end
 
@@ -15,15 +15,21 @@ function mod:PostUpdate()
     local player = Isaac.GetPlayer(0)
     local itemCount = player:GetCollectibleNum(lifeOrb)
     if itemCount > 0 and mustTakeLifeOrbDamage then
-        player:TakeDamage(itemCount, DamageFlag.DAMAGE_NO_PENALTIES | DamageFlag.DAMAGE_RED_HEARTS, EntityRef(player), 0)
+        local i = itemCount - 1
+        while i > 0 do
+            player:TakeDamage(0, DamageFlag.DAMAGE_FAKE, EntityRef(player), 0)
+            i = i - 1
+        end
+        player:TakeDamage(itemCount, DamageFlag.DAMAGE_NO_PENALTIES | DamageFlag.DAMAGE_RED_HEARTS | DamageFlag.DAMAGE_INVINCIBLE, EntityRef(player), 0)
         mustTakeLifeOrbDamage = false
     end
 end
 
 function mod:PostNewLevel()
     local player = Isaac.GetPlayer(0)
+    local playerType = player:GetPlayerType()
     local itemCount = player:GetCollectibleNum(lifeOrb)
-    if itemCount > 0 then
+    if itemCount > 0 and (playerType ~= PlayerType.PLAYER_THELOST and playerType ~= PlayerType.PLAYER_THELOST_B) then
         mustTakeLifeOrbDamage = true
     end
 end
